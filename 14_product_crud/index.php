a0b123e153fb66ba7e58ee5d7429c562b07ae333
shakil <send2shakil@gmail.com>
2021-07-07 16:06:37 +0600

    added partials

--- 14_product_crud/index.php
+++ 14_product_crud/index.php
@@ -19,22 +19,8 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 // echo var_dump($products);
 // echo '</pre>';
 ?>
+<?php include_once 'views/partials/header.php' ?>
 
-<!doctype html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="./styles/app.css" rel="stylesheet">
-    <title>Products CRUD</title>
-</head>
-
-<body>
     <h1>Products CRUD</h1>
     <a href='./create.php' type="button" class="btn btn-lg btn-success">Create Product</a>
     <table class="table">
@@ -108,9 +94,4 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 
         </tbody>
     </table>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-</body>
-
-</html>
+<?php include_once 'views/partials/footer.php' ?>
--- 14_product_crud/update.php
+++ 14_product_crud/update.php
@@ -1,6 +1,6 @@
 <?php
 $id = $_GET['id'] ?? null;
-var_dump($id);
+
 if (!$id) {
     header('Location: index.php');
 }
@@ -10,7 +10,7 @@ $statement = $pdo->prepare("SELECT * FROM products WHERE id = :id");
 $statement->bindValue(':id', $id);
 $statement->execute();
 $product = $statement->fetch(PDO::FETCH_ASSOC);
-var_dump($product);
+
 
 $title = $product['title'] ?? '';
 $description = $product['description'] ?? '';
@@ -73,21 +73,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 ?>
 
-<!doctype html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="./styles/app.css" rel="stylesheet">
-    <title>Create new product</title>
-</head>
-
-<body>
+<?php include_once 'views/partials/header.php' ?>
     <p>
         <a href='index.php' class='btn btn-secondary'>Go back to home</a>
     </p>
@@ -134,9 +120,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-</body>
-
-</html>
+<?php include_once 'views/partials/footer.php' ?>
